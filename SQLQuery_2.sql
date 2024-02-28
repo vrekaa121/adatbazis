602aee341213c2d4b799cb8966de304dcc071061
vrekaa121 <vinkler.reka@gmail.com>
2024-02-28 16:57:21 +0100

    2

--- SQLQuery_2.sql
+++ SQLQuery_2.sql
@@ -8,11 +8,11 @@ WHERE GYERMEK_SZAM = 0 AND UGYFEL_FK = 'laszlo2'
 
 -- feladat2
 
- SELECT szh.TIPUS, YEAR(f.METTOL) AS 'Év', MONTH(f.METTOL) AS 'Hónap', 
+ SELECT szh.TIPUS, YEAR(f.METTOL) AS 'Év', MONTH(f.METTOL) AS 'Hónap', COUNT(*)
  FROM Foglalas f JOIN Szoba sz ON f.SZOBA_FK = sz.SZOBA_ID 
-                JOIN Szallashely szh 
+                JOIN Szallashely szh ON sz.SZALLAS_FK = szh.SZALLAS_ID
  
 
-WHERE >=5 
+WHERE DATEDIFF(DAY, f.METTOL, f.MEDDIG) >=5 
 
- GROUP BY szh.TIPUS, 
+ GROUP BY szh.TIPUS, YEAR(f.METTOL), MONTH(f.METTOL)
